39afbb0798e50720d38c6d1f3d56dfaf74cf2181
gpintof <72316888+gpintof@users.noreply.github.com>
2020-12-04 22:33:02 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Desconfinados</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="css/estilos.css">
+    </head>
+    <body>
+
+    <div style="color:#FF0000" id="central">
+        
+        <h1>alguna imagen</h1>
+    </div>
+</br>
+    <div id="casillero">
+        <h1>datos comuna</h1>
+        <input type="text" name="nombre">
+    </div>
+    </br>
+    <div id="info">
+        <h1>info</h1>
+        <h3>paginas relevantes</h3>
+        </br>
+        <?php echo '<a href="https://www.minsal.cl/">sitio web del Minsal</a>';?>
+        </br>
+        </br>
+        <?php echo '<a href="https://www.gob.cl/coronavirus/pasoapaso/">Plan paso a paso</a>';?>
+        </br>
+        </br>
+    </div>
+    </br>
+    <div id="nacional">
+        <h1>estadistica nacional</h1>
+    </div>
+
+    </body>
+</html>
+
+<?php echo '<a href="https://www.minsal.cl/">Minsal</a>';?>
